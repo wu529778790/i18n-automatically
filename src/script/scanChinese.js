acb5569c5f368f6a428ad9799b30e4110a405032
wuwei <529778790@qq.com>
2024-09-26 11:32:17 +0800

    feat: 增加js替换

--- src/script/scanChinese.js
+++ src/script/scanChinese.js
@@ -8,7 +8,8 @@ const {
   // generate: generateDom,
 } = require("@vue/compiler-dom");
 const { parse: babelParse } = require("@babel/parser");
-const { default: generate } = require("@babel/generator");
+const { default: traverse } = require("@babel/traverse");
+// const { default: generate } = require("@babel/generator");
 const { getConfig } = require("./setting.js");
 const { updateDecorations } = require("./switchLanguage.js");
 const { generateUniqueId, saveObjectToPath } = require("../utils/index.js");
@@ -74,6 +75,16 @@ const generateUUID = (filePath, fileUuid, index, config) => {
   return `${selectedLevels}-${fileUuid}-${index}`;
 };
 
+const getPosition = (code, line, column) => {
+  const lines = code.split("\n");
+  let position = 0;
+  for (let i = 0; i < line - 1; i++) {
+    position += lines[i].length + 1; // +1 for newline character
+  }
+  position += column;
+  return position;
+};
+
 /**
  * 扫描中文
  * @returns {Promise<void>}
@@ -90,6 +101,8 @@ exports.scanChinese = async (filePath) => {
     const fileUuid = generateUniqueId();
 
     const { descriptor } = parseSfc(text);
+    let modifiedText = text;
+    const chineseRegex = /[\u4e00-\u9fa5]/;
     // 解析 Vue 文件
     const template = descriptor.template ? descriptor.template.content : "";
     const script = descriptor.script ? descriptor.script.content : "";
@@ -99,13 +112,23 @@ exports.scanChinese = async (filePath) => {
     // 解析模板
     const templateAst = parseTemplate(template);
 
-    const traverseTemplate = (ast, content) => {
-      let modifiedTemplate = content;
+    // 解析脚本部分
+    const scriptAst = babelParse(script, {
+      sourceType: "module",
+      plugins: ["jsx", "typescript"],
+    });
+
+    const scriptSetupAst = babelParse(scriptSetup, {
+      sourceType: "module",
+      plugins: ["jsx", "typescript"],
+    });
+
+    const traverseTemplate = (ast, template) => {
+      let modifiedTemplate = template;
       // 偏移量
       let offset = 0;
       const traverseNode = (node) => {
         console.log("node", node);
-        const chineseRegex = /[\u4e00-\u9fa5]/;
         switch (node.type) {
           case 0: // Root Node（根节点）
             if (node.children) {
@@ -271,243 +294,58 @@ exports.scanChinese = async (filePath) => {
     };
 
     // 遍历 AST 并生成修改后的模板字符串
-    const modifiedTemplate = traverseTemplate(templateAst, template);
-    // 解析脚本部分
-    // const scriptAst = babelParse(script, {
-    //   sourceType: "module",
-    //   plugins: ["jsx", "typescript"],
-    // });
-
-    // const scriptSetupAst = babelParse(scriptSetup, {
-    //   sourceType: "module",
-    //   plugins: ["jsx", "typescript"],
-    // });
+    if (templateAst) {
+      const modifiedTemplate = traverseTemplate(templateAst, template);
+      modifiedText = modifiedText.replace(template, modifiedTemplate);
+    }
 
     // 遍历脚本AST并修改中文文本
-    // const traverseScript = (ast) => {
-    //   const traverseNode = (node) => {
-    //     const chineseRegex = /[\u4e00-\u9fa5]/;
-    //     if (node.type === "StringLiteral" && chineseRegex.test(node.value)) {
-    //       collectChineseText(node.value);
-    //       node.value = node.value.replace(chineseRegex, fileUuid);
-    //     }
-    //     if (node.type === "TemplateLiteral" && node.quasis) {
-    //       node.quasis.forEach((quasi) => {
-    //         if (chineseRegex.test(quasi.value.raw)) {
-    //           collectChineseText(quasi.value.raw);
-    //           quasi.value.raw = quasi.value.raw.replace(chineseRegex, fileUuid);
-    //         }
-    //       });
-    //     }
-    //     if (node.type === "JSXText" && chineseRegex.test(node.value)) {
-    //       collectChineseText(node.value);
-    //       node.value = node.value.replace(chineseRegex, fileUuid);
-    //     }
-    //     if (node.type === "JSXElement" && node.children) {
-    //       node.children.forEach(traverseNode);
-    //     }
-    //     if (
-    //       node.type === "JSXAttribute" &&
-    //       node.value &&
-    //       node.value.type === "StringLiteral" &&
-    //       chineseRegex.test(node.value.value)
-    //     ) {
-    //       collectChineseText(node.value.value);
-    //       node.value.value = node.value.value.replace(chineseRegex, fileUuid);
-    //     }
-    //     if (
-    //       node.type === "ExpressionStatement" &&
-    //       node.expression &&
-    //       node.expression.type === "CallExpression" &&
-    //       node.expression.arguments
-    //     ) {
-    //       node.expression.arguments.forEach(traverseNode);
-    //     }
-    //     if (node.type === "ObjectExpression" && node.properties) {
-    //       node.properties.forEach(traverseNode);
-    //     }
-    //     if (node.type === "ArrayExpression" && node.elements) {
-    //       node.elements.forEach(traverseNode);
-    //     }
-    //     if (node.type === "FunctionDeclaration" && node.body) {
-    //       traverseNode(node.body);
-    //     }
-    //     if (node.type === "BlockStatement" && node.body) {
-    //       node.body.forEach(traverseNode);
-    //     }
-    //     if (node.type === "ReturnStatement" && node.argument) {
-    //       traverseNode(node.argument);
-    //     }
-    //     if (node.type === "IfStatement" && node.consequent) {
-    //       traverseNode(node.consequent);
-    //     }
-    //     if (node.type === "IfStatement" && node.alternate) {
-    //       traverseNode(node.alternate);
-    //     }
-    //     if (node.type === "ForStatement" && node.body) {
-    //       traverseNode(node.body);
-    //     }
-    //     if (node.type === "WhileStatement" && node.body) {
-    //       traverseNode(node.body);
-    //     }
-    //     if (node.type === "DoWhileStatement" && node.body) {
-    //       traverseNode(node.body);
-    //     }
-    //     if (node.type === "SwitchStatement" && node.cases) {
-    //       node.cases.forEach(traverseNode);
-    //     }
-    //     if (node.type === "SwitchCase" && node.consequent) {
-    //       node.consequent.forEach(traverseNode);
-    //     }
-    //     if (node.type === "TryStatement" && node.block) {
-    //       traverseNode(node.block);
-    //     }
-    //     if (node.type === "CatchClause" && node.body) {
-    //       traverseNode(node.body);
-    //     }
-    //     if (node.type === "VariableDeclaration" && node.declarations) {
-    //       node.declarations.forEach(traverseNode);
-    //     }
-    //     if (node.type === "VariableDeclarator" && node.init) {
-    //       traverseNode(node.init);
-    //     }
-    //     if (node.type === "AssignmentExpression" && node.right) {
-    //       traverseNode(node.right);
-    //     }
-    //     if (node.type === "LogicalExpression" && node.left) {
-    //       traverseNode(node.left);
-    //     }
-    //     if (node.type === "LogicalExpression" && node.right) {
-    //       traverseNode(node.right);
-    //     }
-    //     if (node.type === "ConditionalExpression" && node.consequent) {
-    //       traverseNode(node.consequent);
-    //     }
-    //     if (node.type === "ConditionalExpression" && node.alternate) {
-    //       traverseNode(node.alternate);
-    //     }
-    //     if (node.type === "SequenceExpression" && node.expressions) {
-    //       node.expressions.forEach(traverseNode);
-    //     }
-    //     if (node.type === "UnaryExpression" && node.argument) {
-    //       traverseNode(node.argument);
-    //     }
-    //     if (node.type === "BinaryExpression" && node.left) {
-    //       traverseNode(node.left);
-    //     }
-    //     if (node.type === "BinaryExpression" && node.right) {
-    //       traverseNode(node.right);
-    //     }
-    //     if (node.type === "MemberExpression" && node.object) {
-    //       traverseNode(node.object);
-    //     }
-    //     if (node.type === "MemberExpression" && node.property) {
-    //       traverseNode(node.property);
-    //     }
-    //     if (node.type === "CallExpression" && node.arguments) {
-    //       node.arguments.forEach(traverseNode);
-    //     }
-    //     if (node.type === "NewExpression" && node.arguments) {
-    //       node.arguments.forEach(traverseNode);
-    //     }
-    //     if (node.type === "UpdateExpression" && node.argument) {
-    //       traverseNode(node.argument);
-    //     }
-    //     if (node.type === "AwaitExpression" && node.argument) {
-    //       traverseNode(node.argument);
-    //     }
-    //     if (node.type === "YieldExpression" && node.argument) {
-    //       traverseNode(node.argument);
-    //     }
-    //     if (node.type === "ArrowFunctionExpression" && node.body) {
-    //       traverseNode(node.body);
-    //     }
-    //     if (node.type === "ClassDeclaration" && node.body) {
-    //       traverseNode(node.body);
-    //     }
-    //     if (node.type === "ClassBody" && node.body) {
-    //       node.body.forEach(traverseNode);
-    //     }
-    //     if (node.type === "MethodDefinition" && node.value) {
-    //       traverseNode(node.value);
-    //     }
-    //     if (node.type === "Property" && node.value) {
-    //       traverseNode(node.value);
-    //     }
-    //     if (node.type === "ObjectPattern" && node.properties) {
-    //       node.properties.forEach(traverseNode);
-    //     }
-    //     if (node.type === "ArrayPattern" && node.elements) {
-    //       node.elements.forEach(traverseNode);
-    //     }
-    //     if (node.type === "RestElement" && node.argument) {
-    //       traverseNode(node.argument);
-    //     }
-    //     if (node.type === "AssignmentPattern" && node.right) {
-    //       traverseNode(node.right);
-    //     }
-    //     if (node.type === "SpreadElement" && node.argument) {
-    //       traverseNode(node.argument);
-    //     }
-    //     if (node.type === "TemplateElement" && node.value) {
-    //       traverseNode(node.value);
-    //     }
-    //     if (node.type === "TaggedTemplateExpression" && node.quasi) {
-    //       traverseNode(node.quasi);
-    //     }
-    //     if (node.type === "MetaProperty" && node.meta) {
-    //       traverseNode(node.meta);
-    //     }
-    //     if (node.type === "MetaProperty" && node.property) {
-    //       traverseNode(node.property);
-    //     }
-    //     if (node.type === "ImportDeclaration" && node.specifiers) {
-    //       node.specifiers.forEach(traverseNode);
-    //     }
-    //     if (node.type === "ImportSpecifier" && node.imported) {
-    //       traverseNode(node.imported);
-    //     }
-    //     if (node.type === "ImportDefaultSpecifier" && node.local) {
-    //       traverseNode(node.local);
-    //     }
-    //     if (node.type === "ImportNamespaceSpecifier" && node.local) {
-    //       traverseNode(node.local);
-    //     }
-    //     if (node.type === "ExportNamedDeclaration" && node.declaration) {
-    //       traverseNode(node.declaration);
-    //     }
-    //     if (node.type === "ExportDefaultDeclaration" && node.declaration) {
-    //       traverseNode(node.declaration);
-    //     }
-    //     if (node.type === "ExportAllDeclaration" && node.source) {
-    //       traverseNode(node.source);
-    //     }
-    //     if (node.type === "ExportSpecifier" && node.exported) {
-    //       traverseNode(node.exported);
-    //     }
-    //     if (node.type === "ExportSpecifier" && node.local) {
-    //       traverseNode(node.local);
-    //     }
-    //     if (node.type === "Program" && node.body) {
-    //       node.body.forEach(traverseNode);
-    //     }
-    //   };
+    const traverseScript = (ast, script) => {
+      let modifiedScript = script;
+      traverse(ast, {
+        StringLiteral(path) {
+          console.log("path", path);
+          if (chineseRegex.test(path.node.value)) {
+            console.log(path.node.value);
+            const uuid = generateUUID(filePath, fileUuid, index, config);
+            const start = path.node.loc.start;
+            const end = path.node.loc.end;
+            const startPos = getPosition(
+              modifiedScript,
+              start.line,
+              start.column
+            );
+            const endPos = getPosition(modifiedScript, end.line, end.column);
+            const replacement = `${config.scriptI18nCall}('${uuid}')`;
+            modifiedScript =
+              modifiedScript.substring(0, startPos) +
+              replacement +
+              modifiedScript.substring(endPos);
+            index++;
+            collectChineseText(uuid, path.node.value);
+          }
+        },
+      });
 
-    //   traverseNode(ast);
-    // };
+      return modifiedScript;
+    };
 
-    // traverseScript(scriptAst);
-    // traverseScript(scriptSetupAst);
+    if (scriptAst && scriptAst.program && scriptAst.program.body.length > 0) {
+      const modifiedScript = traverseScript(scriptAst, script);
+      modifiedText = modifiedText.replace(scriptSetup, modifiedScript);
+    }
 
-    // const scriptCode = generate(scriptAst).code;
-    // const scriptSetupCode = generate(scriptSetupAst).code;
+    if (
+      scriptSetupAst &&
+      scriptSetupAst.program &&
+      scriptSetupAst.program.body.length > 0
+    ) {
+      const modifiedScript = traverseScript(scriptSetupAst, scriptSetup);
+      modifiedText = modifiedText.replace(scriptSetup, modifiedScript);
+    }
 
-    // 替换原模板内容
-    const newText = text.replace(template, modifiedTemplate);
-    // .replace(script, scriptCode)
-    // .replace(scriptSetup, scriptSetupCode);
     // 保存文件
-    await saveFileContent(filePath, newText);
+    await saveFileContent(filePath, modifiedText);
     // 保存语言包
     const obj = Object.fromEntries(chineseTexts);
     await saveObjectToPath(obj, `${config.i18nFilePath}/locale/zh.json`);
