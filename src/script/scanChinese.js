da97bc3496461da8685ecc32f2f0f014f4208f66
wuwei <529778790@qq.com>
2024-10-10 14:44:08 +0800

    chore: Refactor scanChinese function for improved readability and maintainability

--- src/script/scanChinese.js
+++ src/script/scanChinese.js
@@ -7,15 +7,11 @@ const { updateDecorations } = require("./switchLanguage.js");
  * @param {string} filePath 文件路径
  */
 exports.scanChinese = async (filePath = undefined) => {
-  try {
-    if (!filePath) {
-      filePath = vscode.window.activeTextEditor.document.uri.fsPath;
-    }
-    await processFile(filePath);
-    setTimeout(() => {
-      updateDecorations();
-    }, 300);
-  } catch (error) {
-    console.error(`发生未知错误：${error}`);
+  if (!filePath) {
+    filePath = vscode.window.activeTextEditor.document.uri.fsPath;
   }
+  await processFile(filePath);
+  setTimeout(() => {
+    updateDecorations();
+  }, 300);
 };
