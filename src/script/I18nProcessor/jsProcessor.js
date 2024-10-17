4894fc112d300df228586db8f74a86d483a4293e
mengmingxing <mengmingxing@fadada.com>
2024-10-17 17:47:35 +0800

    fix bug

--- src/script/I18nProcessor/jsProcessor.js
+++ src/script/I18nProcessor/jsProcessor.js
@@ -93,7 +93,6 @@ function handleChineseString(path, context, isTemplateLiteral = false) {
     }
 
     const key = generateKey(context);
-    context.translations.set(key, value.trim());
 
     if (isTemplateLiteral) {
       handleTemplateLiteral(path, context, key);
@@ -105,7 +104,9 @@ function handleChineseString(path, context, isTemplateLiteral = false) {
     } else {
       replaceWithI18nCall(path, context, key);
     }
+    context.translations.set(key, value.trim());
   } catch (error) {
+    context.index--;
     customConsole.error('handleChineseString 中出错:', error);
   }
 }
